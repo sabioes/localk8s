2ffc47fd64fa6dadf31acabe012c6d8dd3700386
Carlos Silva <sabioes@gmail.com>
2023-08-31 20:17:13 +0200

    debug initial ansible

--- localk8s.sh
+++ localk8s.sh
@@ -27,8 +27,8 @@ case ${COMMAND} in
     startcontainer)
         echo "starting docker container"
         #docker run --rm -it localk8s:latest
-        #docker run --network bridge -v "${PWD}":/work:ro -v "${PWD}"/ansible/roles:/root/.ansible/roles -v ~/.ssh:/root/.ssh:ro --rm localk8s:latest ansible-playbook ansible/install_virtualbox.yml -i ansible/hosts/hosts.ini 
-        docker run --network bridge -v "${PWD}":/work -v "${PWD}"/ansible/roles:/root/.ansible/roles -v ~/.ssh:/root/.ssh:rw --rm -it localk8s:latest
+        #docker run --network bridge -v "${PWD}":/work:ro -v "${PWD}"/ansible/roles:/root/.ansible/roles -v ~/.ssh:/root/.ssh:ro --rm localk8s:latest ansible-playbook ansible/install_k8s.yml -i ansible/hosts/hosts.ini 
+        docker run --network bridge -v "${PWD}":/work -v "${PWD}"/ansible/roles:/root/.ansible/roles -v ~/.ssh:/root/.ssh:rw --add-host host.docker.internal:host-gateway --rm -it localk8s:latest 
     ;;
 
     anhelp)
